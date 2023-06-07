710305e0a7dcd2ceedf93463afc05277e13779d8
Eze <Eze@DESKTOP-PGAGFI4>
2023-06-07 13:55:02 +0100

    Add project files.

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿//Program starts here
+/*This Is to print Hello World
+ */
+Console.WriteLine("Hello, Eze!");
+
+
+
+
+
+//Declaration of variables
+string studentName;
+int studentAge;
+bool studentPresent;
+char studentSex;
+double studentFee;
+
+//initializing or assinging values to  the variables
+studentName = "Eze";
+studentAge = 20;
+studentPresent = true;
+studentSex = 'M';
+studentFee = 400.89;
+
+//Basic operations with the variables (+,-,*,/,%)
+//Addition 
+studentAge += 17;
+Console.WriteLine("the new age is " + studentAge + "years");
+
+Console.ReadLine();
